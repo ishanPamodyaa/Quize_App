ef8f1492ea48d0cff29855e5dd98b012ddbda73e
ishanPamodyaa <inishan9999@gmail.com>
2025-03-15 11:37:46 +0530

    first commit

---
+++ src/main/java/edu/icet/dto/Exam.java
@@ -0,0 +1,12 @@
+package edu.icet.dto;
+
+public class Exam {
+    private Integer id;
+    private String examName;
+    private String description;
+    private Integer courseId;
+    private String timeLimit;
+    private Integer passingScore;
+    private Integer questionCount;
+
+}
